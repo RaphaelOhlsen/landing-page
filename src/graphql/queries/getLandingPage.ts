a75445d8eae9915d41a3a38b7bd682a3bb3f152b
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2020-08-21 16:13:44 -0300

    Tornando o header dinamico

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,30 @@
 const GET_LANDING_PAGE = /* GraphQL */ `
-  query GET_LANDING_PAGE {
-    landingPage {
-      logo {
-        alternativeText
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
         url
       }
+      image {
+        url
+        alternativeText
+      }
+    }
+  }
+
+  fragment logo on LandingPage {
+    logo {
+      alternativeText
+      url
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
     }
   }
 `
