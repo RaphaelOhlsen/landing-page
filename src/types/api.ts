9d8c0c5ab861d9286bc29264e95570d05e6fdc51
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2020-08-24 11:18:14 -0300

    Tornando o SectionModules dinâmico

--- src/types/api.ts
+++ src/types/api.ts
@@ -10,6 +10,13 @@ export type Concept = {
   title: string
 }
 
+export type Module = {
+  id: string
+  title: string
+  subtitle: string
+  description: string
+}
+
 export type LogoProps = {
   alternativeText: string
   url: string
@@ -47,10 +54,16 @@ export type SectionConceptsProps = {
   concepts: Concept[]
 }
 
+export type SectionModulesProps = {
+  title: string
+  modules: Module[]
+}
+
 export type LandingPageProps = {
   logo: LogoProps
   header: HeaderProps
   sectionAboutProject: SectionAboutProjectProps
   sectionTech: SectionTechProps
   sectionConcepts: SectionConceptsProps
+  sectionModules: SectionModulesProps
 }
